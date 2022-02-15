98a6a1c5509dda674efe11d8d0acbba41cd3afac
“[Nicholas <[n.samaras4@outlook.com]>
2022-02-15 11:48:57 -0500

    update

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios' 
 
-const API_URL = '/api/users'
+const API_URL = '/api/users/'
 
 
 // Register User
@@ -16,9 +16,20 @@ const register = async (userData) => {
 // Logout user
 const logout = () => localStorage.removeItem('user')
 
+// Login user
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData)
+
+    if(response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+    return response.data
+}
+
 const authService = {
     register,
-    logout
+    logout,
+    login
 }
 
 export default authService
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
 import {FaSignInAlt} from 'react-icons/fa'
 import {toast} from 'react-toastify'
 import {useSelector, useDispatch} from 'react-redux'
-import {login} from '../features/auth/authSlice'
+import {login, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -16,10 +18,24 @@ function Login() {
 
   
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-  const {user, isLoading, isSuccess, message} = useSelector(
+  const {user, isLoading, isError,  isSuccess, message} = useSelector(
     (state) => state.auth
   )
+
+  useEffect(() => {
+    if(isError) {
+      toast.error(message)
+    }
+
+    // Redirect when logged in
+    if(isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [isError, isSuccess, user, message, navigate, dispatch])
   
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -37,6 +53,10 @@ function Login() {
     }
 
     dispatch(login(userData))
+  }
+
+  if(isLoading) {
+    return <Spinner />
   }
 
   return (
