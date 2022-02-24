e0f249ffbdb91c05acf05f41130920f780c4ad25
“[Nicholas <[n.samaras4@outlook.com]>
2022-02-24 10:21:20 -0500

    update

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -4,6 +4,7 @@ import {useSelector, useDispatch} from 'react-redux'
 import {getTickets, reset} from '../features/tickets/ticketSlice'
 import Spinner from '../components/Spinner'
 import BackButton from '../components/BackButton'
+import TicketItem from '../components/TicketItem'
 
 function Tickets() {
     const {tickets, isLoading, isSuccess} = useSelector(
@@ -29,9 +30,21 @@ function Tickets() {
     }
 
   return (
-    <div>
+    <>
+        <BackButton url='/' />
         <h1>Tickets</h1>
-    </div>
+        <div className="tickets">
+            <div className="ticket-headings">
+                <div>Date</div>
+                <div>Status</div>
+                <div>Product</div>
+                <div></div>
+            </div>
+            {tickets.map((ticket) => (
+                <TicketItem key={ticket._id} ticket={ticket} />
+            ))}
+        </div>
+    </>
   )
 }
 
