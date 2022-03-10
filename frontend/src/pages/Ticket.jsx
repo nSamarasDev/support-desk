a8c82db9539798f4f1e268945e2783b059531df2
“[Nicholas <[n.samaras4@outlook.com]>
2022-03-10 10:28:54 -0500

    update

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -14,16 +14,16 @@ import NoteItem from'../components/NoteItem'
 
 const customStyles = {
     content: {
-        width: '60px',
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate{-50%, -50%)',
-        position: 'relative',
+      width: '600px',
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      position: 'relative',
     },
-}
+  }
 
 Modal.setAppElement('#root')
 
